ebcfd4d7475b8b6e900036184571516ceeb8869f
krilop <krilop.gamer@gmail.com>
2024-03-07 09:35:54 +0300

    divided on modules. log4j2 was connected and configured. build.yml was changed for working with maven

--- JDBC/src/main/java/org/example/Main.java
+++ JDBC/src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
         DBFunctions db = new DBFunctions();
